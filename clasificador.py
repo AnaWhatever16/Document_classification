b734a068c80b52a8725fea0ca367a92c21a82ae7
AnaWhatever16 <anacasadofauli@gmail.com>
2020-12-20 18:41:36 +0100

    So this was stupid and wrong. I lost several hours of my life for nothing and I feel like I am useless. I want to cry.

--- clasificador.py
+++ clasificador.py
@@ -23,6 +23,7 @@ nmin = 16
 rango = 15
 glosario = ""
 modelo = 0
+resultados = ""
 STOP_WORDS = []
 
 temas = ["Deportes", "Politica", "Salud"]
@@ -48,7 +49,6 @@ def clasificador_documentos(directorio, n_min, rango, glosario, modelo):
         
         train_docs = []
         for j in range(0, n_min-1):
-            #print(path + i.lower() + str(j+1) + ".txt")
             f = open(path + i.lower() + str(j+1) + ".txt","r")
             files = f.read()
            
@@ -59,30 +59,40 @@ def clasificador_documentos(directorio, n_min, rango, glosario, modelo):
         train_bow = process_text_doc2bow(train_docs, dictionary)  
         modelos_tema[i], sim_tema[i] = crear_modelos(train_bow, dictionary, modelo)
 
-        test_docs = []
-        #Documentos para test 
-        for j in range(n_min, n_min + rango -1):
-            #print(path + i.lower() + str(j+1) + ".txt")
-            f = open(path + i.lower() + str(j+1) + ".txt","r")
-            files = f.read()
-           
-            #Se almacenan todos los documentos en una lista para poder procesarlos conjuntamente
-            test_docs += [files]   
-       
-    # Dependiendo del modelo a utilizar se llamará a las funciones X_model
-    lanzar_clasificador(test_docs, dictionary, modelos_tema, sim_tema)
+    #Documentos para test 
+    res = open(resultados,"r+")
+    res.truncate(0)
+    res.close()
 
+    for i in temas:
+        path = directorio + "/Documentos/" + i + "/"
+        for j in range(n_min-1, n_min + rango -1):
+            doc_path = path + i.lower() + str(j+1) + ".txt"
+            test_doc=""
+            f = open(doc_path,"r")
+            test_doc = f.read()
+            f.close()
+            values={}
+            # Dependiendo del modelo a utilizar se llamará a las funciones X_model
+            values, print_thing = lanzar_clasificador(test_doc, dictionary, modelos_tema, sim_tema)
+            guardar_resultados(resultados, doc_path, values)
+
+        print(print_thing)
     
-def guardar_resultados():
+def guardar_resultados(path_results, path_doc, values):
     # Guardar txt con los documentos y los 3 valores para cada glosario
     # Mostrar por pantalla
     # Por modelo, ppppprecisión (relevantes/recuperados) y exhaustividad (los que son/los relevantes)
     # Dibujitos
+
+    res = open(path_results, "a")
+    res.write(path_doc + "\t" + str(values)+"\n")
+    res.close()
     pass
 
 def tfidf_model(bow, dictionary):     
     tfidf = models.TfidfModel(bow)
-    index = similarities.SparseMatrixSimilarity(bow, num_features=len(dictionary))
+    index = similarities.MatrixSimilarity(bow, num_features=len(dictionary))
     return tfidf, index
            
            
@@ -132,9 +142,9 @@ def create_dictionary(path_glosario, pathname= None):
     for filename in os.listdir(path_glosario):
         f2 = open(path_glosario + filename, "r")
         glosario = f2.read()
-        glosario_clean = clean_docs(glosario) ## GUARDAR LOS TRES DICCIONARIOS JUNTOS!!!
-        #glosario_clean = process_text_doc2bow(glosario)
-        dictionary.add_documents(glosario_clean)
+        #glosario_clean =  wordpunct_tokenize(glosario) ## GUARDAR LOS TRES DICCIONARIOS JUNTOS!!!
+        tokens = [word for word in glosario.split()]
+        dictionary.add_documents([tokens])
 
     if pathname:
         dictionary.save(pathname+"/corpus.dict")
@@ -148,35 +158,37 @@ def create_bow_from_dict(corpus, dictionary, pathname=None):
     return bow
 
 
-#La función launch_glosario hará la comparación entre el glosario y los documentos
-def launch_glosario_tfidf(glosario, tfidf, bow, dictionary):
-    glosario = tfidf[dictionary.doc2bow(glosario)]
+# #La función launch_glosario hará la comparación entre el glosario y los documentos
+# def launch_glosario_tfidf(glosario, tfidf, bow, dictionary):
+#     glosario = tfidf[dictionary.doc2bow(glosario)]
     
-    return enumerate(index[glosario])# key=itemgetter(1), reverse=True)
+#     return enumerate(index[glosario])# key=itemgetter(1), reverse=True)
     
     
 #Dependiendo del valor de la variable modelo, la función crear_modelos utilizará la llamada al proceso correspondiente    
 def crear_modelos(train_bow, dictionary, m):
-    if(m == 0):
-        model, index = tfidf_model(train_bow, dictionary)
+    #if(m == 0):
+    model, index = tfidf_model(train_bow, dictionary)
         #Crear diccionario con glosario
-    if(m == 1):
-        word2vec_model(bow, glosario, dictionary, path_glosario)
-    if(m == 2):
-        naivebayes_model(bow, glosario, dictionary, path_glosario)
+    # if(m == 1):
+    #     word2vec_model(train_bow, glosario, dictionary, path_glosario)
+    # if(m == 2):
+    #     naivebayes_model(train_bow, glosario, dictionary, path_glosario)
     
     return model, index
 
-def lanzar_clasificador(test_docs, dictionary, modelos_tema, sim_tema):
-    for doc in test_docs:
+def lanzar_clasificador(test_doc, dictionary, modelos_tema, sim_tema):
+    # for doc in test_docs:
         #Pre-procesamiento del documento de test    
-        test_bow = process_text_doc2bow(doc, dictionary) 
-        for i in temas:
-            tfidf = modelos_tema[i]
-            index = sim_tema[i]
-            doc_tfidf = tfidf[test_bow]
-            sim = index[doc_tfidf]
-    print("END")
+    test_bow = process_text_doc2bow([test_doc], dictionary) 
+    values = {}
+    for i in temas:
+        tfidf = modelos_tema[i]
+        index = sim_tema[i]
+        doc_tfidf = tfidf[test_bow[0]]
+        values[i] = index[doc_tfidf]
+
+    return values, index[doc_tfidf]
    
 ######################
 # PROGRAMA PRINCIPAL #
@@ -210,6 +222,11 @@ parser.add_argument('-m',
                     type=int,
                     help="Modelo a utilizar para el clasificador. 0 = VSM con tf-idf, 1 = VSM (word2vec), 2 = Naive Bayes")
 
+parser.add_argument('-v',
+                    "--valores",
+                    type=str,
+                    help = "Path al documento donde guardar los los resultados")
+
                     
 
 # Parseo de los argumentos
@@ -218,7 +235,7 @@ arguments = vars(parser.parse_args())
 if arguments['directorio']:
     directorio = arguments['directorio']
 else:
-    print("ERROR: Porfavor introduzca palabras válidas")
+    print("ERROR: Porfavor introduzca palabras válidas para directorio")
     exit()
 if arguments['nmin']:
     if arguments['nmin'] > 0:
@@ -243,6 +260,11 @@ if arguments['modelo']:
     else:
         print("ERROR: Introduzca un valor válido mayor que 0 y menor que 2 para un modelo válido")
         exit()
+if arguments['valores']:
+    resultados = arguments['valores']
+else:
+    print("ERROR: Porfavor introduzca palabras válidas para resultados")
+    exit()
 
 clasificador_documentos(directorio, nmin, rango, glosario, modelo)
 
